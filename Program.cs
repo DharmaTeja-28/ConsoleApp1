38908d8fb7c5832dbb8eb72c63b0188c866d016e
DharmaTeja-28 <imdharma28@gmail.com>
2021-09-15 13:50:23 +0530

    j

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!")
             Console.WriteLine("This is Dharma");
             Console.WriteLine("Bangalore");
             Console.WriteLine("This is Teja");
